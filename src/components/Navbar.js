bce8ecd21cb65b0cafb0071c7201dd4d594b7c0f
jasmina001 <jorayevajasmina76@gmail.com>
2025-04-06 00:01:37 +0500

    navbar section + rwd

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,56 @@
 // src/components/Navbar.js
 
-import React from 'react';
-import './Navbar.css'; // CSS faylini alohida qo'shamiz
+import React, { useState } from 'react';
+import './Navbar.css';
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
         <a href="/" className="navbar-logo">
           Hamkor Bank
         </a>
-        <ul className="navbar-links">
+        <ul className={`navbar-links ${menuOpen ? 'active' : ''}`}>
           <li className="navbar-item">
-            <a href="/" className="navbar-link">Главная</a>
+            <a href="/for-investors/facts" className="navbar-link">Существенные факты</a>
           </li>
           <li className="navbar-item">
             <a href="/for-investors/reports" className="navbar-link">Отчеты</a>
           </li>
           <li className="navbar-item">
-            <a href="/for-investors/about" className="navbar-link">О компании</a>
+            <a href="/for-investors/emission" className="navbar-link">Эмиссия</a>
+          </li>
+  
+          <li className="navbar-item">
+            <a href="/for-investors/repo" className="navbar-link">Перекупленные акции</a>
           </li>
           <li className="navbar-item">
-            <a href="/for-investors/contact" className="navbar-link">Контакты</a>
+            <a href="/for-investors/buy" className="navbar-link buy-link">
+              <span className="currency">$</span> Купить <span>1200$</span>
+            </a>
+          </li>
+          <li className="navbar-item">
+            <a href="/for-investors/sell" className="navbar-link sell-link">
+              <span className="currency">$</span> Продать
+              <span>13 800</span>
+            </a>
+          </li>
+          <li className="navbar-item">
+            <a href="/for-investors/internet-bank" className="navbar-link">Интернет-банк</a>
           </li>
         </ul>
+        {/* Hamburger Icon */}
+        <div className="hamburger-icon" onClick={toggleMenu}>
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </div>
       </div>
     </nav>
   );
